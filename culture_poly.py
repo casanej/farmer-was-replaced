ae3e116e22dea7621a768f8bddca0f2d060919b9
Rafael Casanje <rafael.casanje@gmail.com>
2024-06-03 00:05:55 -0300

    changes on logic to plant

--- culture_poly.py
+++ culture_poly.py
@@ -1,4 +1,4 @@
-from routine_plant import routine_harvest_execute, routine_soil_execute, routine_plant_execute
+from routine_plant import routine_harvest_execute, routine_soil_execute, routine_plant_execute, routine_plant_plan_execute
 from routine_resource import routine_get_carrot_seed, routine_get_pumpkin_seed, routine_get_tank, routine_get_sunflower
 
 def culture_poly(rows=1, columns=1, grass=0, bush=0, carrot=0, pumpkin=0, tree = 0, sunflower = 0, cactus = 0):
@@ -15,39 +15,49 @@ def culture_poly(rows=1, columns=1, grass=0, bush=0, carrot=0, pumpkin=0, tree =
 		print("Error: Not enough spaces for the plants.")
 		return
 
+	clear()
+
 	plantPattern = []
 
 	for x in range(rows):
 		plantRow = []
 		for y in range(columns):
-			if grass > 0:
-				plantRow.append(Entities.Grass)
-				grass -= 1
-			elif bush > 0:
-				plantRow.append(Entities.Bush)
-				bush -= 1
-			elif carrot > 0:
-				plantRow.append(Entities.Carrots)
-				carrot -= 1
-			elif pumpkin > 0:
-				plantRow.append(Entities.Pumpkin)
-				pumpkin -= 1
-			elif tree > 0:
-				plantRow.append(Entities.Tree)
-				tree -= 1
-			elif sunflower > 0:
-				plantRow.append(Entities.Sunflower)
-				sunflower -= 1
-			elif cactus > 0:
-				plantRow.append(Entities.Cactus)
-				cactus -= 1
-			else:
-				plantRow.append(Entities.Grass)
-
+			plantRow.append(None)
 		plantPattern.append(plantRow)
 
-	clear()
-	do_a_flip()
+	allSpacesFilled = False
+	positionFilled = 0
+
+	while not allSpacesFilled:
+		if pumpkin > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Pumpkin)
+			pumpkin -= 1
+		elif tree > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Tree)
+			tree -= 1
+		elif bush > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Bush)
+			bush -= 1
+		elif carrot > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Carrots)
+			carrot -= 1
+		elif sunflower > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Sunflower)
+			sunflower -= 1
+		elif cactus > 0:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Cactus)
+			cactus -= 1
+		else:
+			plantPattern = routine_plant_plan_execute(rows, columns, plantPattern, Entities.Grass)
+
+		positionFilled += 1
+
+		if positionFilled == spaces:
+			allSpacesFilled = True
+
+	routine_get_carrot_seed(carrot, carrot)
+	routine_get_pumpkin_seed(pumpkin, pumpkin)
+	routine_get_sunflower(sunflower, sunflower)
 
 	for x in range(rows):
 			for y in range(columns):
--- routine_plant.py
+++ routine_plant.py
@@ -25,3 +25,72 @@ def routine_plant_execute(entity = Entities.Grass):
     else:
       print("Cannot plant on this ground type")
 
+def routine_plant_plan_execute(cols, rows, plantPattern, entity):
+  bestPositionX = 0
+  bestPositionY = 0
+  bestPositionScore = -1
+
+  for initialBestY in range(rows):
+    for initialBestX in range(cols):
+      if plantPattern[initialBestY][initialBestX] == None:
+        bestPositionX = initialBestX
+        bestPositionY = initialBestY
+        bestPositionScore = 0
+        break
+
+  if (entity == Entities.Pumpkin or entity == Entities.Tree):
+    for lookupBestY in range(rows):
+      for lookupBestX in range(cols):
+
+        if (lookupBestX + 1 > rows):
+          posRight = plantPattern[lookupBestY][lookupBestX+1]
+        else:
+          posRight = None
+
+        if (lookupBestX - 1 >= 0):
+          posLeft = plantPattern[lookupBestY][lookupBestX-1]
+        else:
+          posLeft = None
+
+        if (lookupBestY + 1 > cols):
+          posTop = plantPattern[lookupBestY+1][lookupBestX]
+        else:
+          posTop = None
+
+        if (lookupBestY - 1 >= 0):
+          posBottom = plantPattern[lookupBestY-1][lookupBestX]
+        else:
+          posBottom = None
+
+        bestPositionLocalScore = 0
+
+        if (entity == Entities.Pumpkin):
+          if posTop == Entities.Pumpkin:
+            bestPositionLocalScore += 1
+          if posRight == Entities.Pumpkin:
+            bestPositionLocalScore += 1
+          if posBottom == Entities.Pumpkin:
+            bestPositionLocalScore += 1
+          if posLeft == Entities.Pumpkin:
+            bestPositionLocalScore += 10
+        elif (entity == Entities.Tree):
+          if posTop != Entities.Tree:
+            bestPositionLocalScore += 1
+          if posRight != Entities.Tree:
+            bestPositionLocalScore += 1
+          if posBottom != Entities.Tree:
+            bestPositionLocalScore += 1
+          if posLeft != Entities.Tree:
+            bestPositionLocalScore += 1
+
+        if (bestPositionLocalScore > bestPositionScore) and plantPattern[lookupBestY][lookupBestX] == None:
+          bestPositionScore = bestPositionLocalScore
+          bestPositionY = lookupBestY
+          bestPositionX = lookupBestX
+
+  plantPattern[bestPositionY][bestPositionX] = entity
+
+  return plantPattern
+
+
+
